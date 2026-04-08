11a669a945e826c3d7b75dd8b24989cf55e41c4a
NexxCod <drmsalinasv@gmail.com>
2026-04-07 20:06:45 -0400

    habilitación de CORS

--- routes/googleAuthRoutes.js
+++ routes/googleAuthRoutes.js
@@ -1,19 +1,12 @@
 import express from "express";
-import { protect } from "../middleware/authMiddleware.js";
 import {
   getGoogleAuthUrl,
   handleGoogleAuthCallback,
 } from "../controllers/googleAuthController.js";
-import {
-  createDriveUploadSession,
-  finalizeDriveUpload,
-} from "../controllers/googleDriveUploadController.js";
 
 const router = express.Router();
 
 router.get("/auth/url", getGoogleAuthUrl);
 router.get("/auth/callback", handleGoogleAuthCallback);
-router.post("/drive/upload-session", protect, createDriveUploadSession);
-router.post("/drive/finalize", protect, finalizeDriveUpload);
 
 export default router;
